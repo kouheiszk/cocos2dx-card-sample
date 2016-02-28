d8bef8a0d69686c6017930407a9ce0767288f9f0
Kouhei Suzuki <kouhei.szk@gmail.com>
2016-02-29 01:23:39 +0900

    カードの配置を修正

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -49,29 +49,46 @@ bool Game::init()
 
 void Game::touchTrumpCards()
 {
+    Size visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+    
     // カードの番号の取りうる値を設定する
     
     // numbersをシャッフルする
     
     // カードを4x4に配置する
-    for (int x = 0; x < 4; x++) {
-        for (int y = 0; y < 4; y++) {
+    int xMax = 4;
+    int yMax = 4;
+    float cardMargin = 10;
+    
+    for (int x = 0; x < xMax; x++) {
+        for (int y = 0; y < yMax; y++) {
             //ランダムで1つの値を取得する
             int index = rand() % 52 + 1;
             
             //カードを生成する
             std::string normalCardImageName = StringUtils::format("images/trump/trump%03d.png", index);
             std::string highlightedCardImageName = StringUtils::format("images/trump/trump%03d.png", index);
-            
             auto trumpCard = MenuItemImage::create(normalCardImageName,
                                                    highlightedCardImageName,
                                                    CC_CALLBACK_1(Game::trumpCardButtonCallback, this));
-            trumpCard->setPosition(Vec2((x + 0.5) * 30 + 160, (y + 0.5) * 30));
-            trumpCard->setScaleX(0.2);
-            trumpCard->setScaleY(0.2);
+            
+            double xCardScale = ((visibleSize.width - cardMargin * (xMax + 1)) / xMax) / trumpCard->getContentSize().width;
+            double yCardScale = ((visibleSize.height - cardMargin * (yMax + 1)) / yMax) / trumpCard->getContentSize().height;
+            double cardScale = MIN(xCardScale, yCardScale);
+            Size cardSize = Size(trumpCard->getContentSize().width * cardScale,
+                                 trumpCard->getContentSize().height * cardScale);
+            trumpCard->setScaleX(cardScale);
+            trumpCard->setScaleY(cardScale);
+            
+            trumpCard->setPosition(Point(origin.x + cardSize.width / 2 + x * cardSize.width + cardMargin * (x + 1),
+                                         origin.y + cardSize.height / 2 + y * cardSize.height + cardMargin * (y + 1)));
+            
+            
             auto trumpCardMenu = Menu::create(trumpCard, NULL);
             trumpCardMenu->setPosition(Vec2::ZERO);
             trumpCardMenu->setTag(index);
+            
             this->addChild(trumpCardMenu, 2);
         }
     }
